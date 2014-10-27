ce069959b35e5c9ae0424a80325333f645cc1dcc
Félix Saparelli <felix@passcod.name>
2014-10-27 19:06:36 +1300

    Document canvas mod

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,14 +1,25 @@
+#![stable]
+
 use libc;
 use std;
 use cairo;
 use super::content::Content;
 
+/// Opaque struct which holds a reference to the underlying Clutter object.
 #[repr(C)]
 pub struct CanvasRef {
   opaque: *mut libc::c_void
 }
 
 impl CanvasRef {
+  /// Creates a new instance of Canvas.
+  ///
+  /// You should call `.set_size()` to set the size of the canvas.
+  ///
+  /// You should call `.invalidate()` every time you wish to draw the contents
+  /// of the canvas.
+  ///
+  /// _Since 1.10_
   pub fn new() -> CanvasRef {
     unsafe {
       let foreign_result = clutter_canvas_new();
@@ -17,15 +28,39 @@ impl CanvasRef {
   }
 }
 
+/// Content for 2D painting
+///
+/// The Canvas class is a Content implementation that allows drawing using the
+/// Cairo API on a 2D surface.
+///
+/// In order to draw on a Canvas, you should connect a handler to the `draw`
+/// signal; the signal will receive a cairo context that can be used to draw.
+/// Canvas will emit the `draw` signal when invalidated using `.invalidate()`.
+///
+/// _Since 1.10_
 pub trait Canvas {
+  /// Returns a pointer to the the underlying C object.
+  ///
+  /// Generally only used internally.
   fn as_canvas(&self) -> *mut libc::c_void;
 
+  /// Sets the size of the canvas, and invalidates the content.
+  ///
+  /// This method will cause the canvas to be invalidated only if the size of
+  /// the canvas surface has changed.
+  ///
+  /// If you want to invalidate the contents of the canvas when setting the
+  /// size, you can use the return value of the method to conditionally call
+  /// `.invalidate()`.
+  ///
+  /// _Since 1.10_
   fn set_size(&mut self, width: i32, height: i32) {
     unsafe {
       clutter_canvas_set_size(self.as_canvas(), width, height);
     }
   }
 
+  //FIXME: doc
   fn on_draw(&mut self, handler: &|&mut CanvasRef, &mut cairo::Cairo, i32, i32| -> bool) -> u64 {
     unsafe {
       let null_void: *mut libc::c_void = std::ptr::null_mut();
@@ -46,6 +81,7 @@ impl Content for CanvasRef {
   }
 }
 
+//FIXME: doc
 extern "C" fn handler_for_on_draw(canvas: *mut libc::c_void, cairo: *mut libc::c_void, width: i32, height: i32, handler: *mut libc::c_void) -> i32 {
   unsafe {
     let mut canvas_r = CanvasRef { opaque: canvas };
