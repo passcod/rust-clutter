30cf81cea318babeef1e02b1618971554f7bea8e
Félix Saparelli <felix@passcod.name>
2014-10-27 20:59:45 +1300

    Document scaling mod

--- src/scaling.rs
+++ src/scaling.rs
@@ -1,6 +1,16 @@
+#![stable]
+
+/// The scaling filters to be used with the `minification-filter` and
+/// `magnification-filter` properties.
 #[repr(i32)]
 pub enum Filter {
+  /// Linear interpolation filter
   Linear = 0,
+
+  /// Nearest neighbor interpolation filter
   Nearest = 1,
+
+  /// Trilinear minification filter, with mipmap generation; this filter
+  /// linearly interpolates on every axis, as well as between mipmap levels.
   Trilinear = 2
 }
