3bc474d1273203cc5e43dd3eb784b624953ed1a5
Félix Saparelli <felix@passcod.name>
2014-10-27 17:06:19 +1300

    Expand the project description, add license notices

--- README.md
+++ README.md
@@ -4,3 +4,28 @@ _Rust bindings for [Clutter]._
 
 [Clutter]: https://developer.gnome.org/clutter/
 [![Build Status](https://travis-ci.org/passcod/rust-clutter.svg?branch=master)](https://travis-ci.org/passcod/rust-clutter)
+
+The bulk of the bindings were original pulled from [clutter.rs], but were then
+manually fixed to work with the latest Rust nightly. However, this binding
+isn't just that: the full API documentation has been copied into the relevant
+places, and altered in some places to fit with the way Rust works (e.g. no NULL
+pointers!), and the `rustdoc`-generated files should be all one needs to create
+an application using Clutter.
+
+Stability levels are also taken from the upstream documentation and applied with
+`#[stable]` and `#[deprecated]` tags. The binding is set as stable globally, so
+reading through the code, one should assume that unmarked methods are stable.
+
+The version number in `Cargo.toml` track the upstream version number. The
+version of the bindings can only be tracked with git SHA-1s at the moment.
+
+[clutter.rs]: https://github.com/jensnockert/clutter.rs
+
+## Legal
+
+- Clutter is licensed under the LGPL, and thus these bindings are, too.
+- The clutter documentation, and thus the rustdoc comments which contain text
+  copied or modified from [the clutter documentation][Clutter], is licensed
+  under the GNU Free Documentation License.
+- Anything that is not covered by either of the above is released in the
+  [Public Domain](https://passcod.name/PUBLIC.txt) unless specified.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+//! Clutter is a GObject based library for creating fast, visually rich,
+//! graphical user interfaces.
+//!
+//! ---------------------------------------------------------------------------
+//!
+//! This library is free software; you can redistribute it and/or modify it
+//! under the terms of the GNU Library General Public License as published by
+//! the Free Software Foundation; either version 2 of the License, or (at your
+//! option) any later version.
+//!
+//! This library is distributed in the hope that it will be useful, but WITHOUT
+//! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//! FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public
+//! License for more details.
+//!
+//! You may obtain a copy of the GNU Library General Public License from the
+//! Free Software Foundation by visiting their Web site or by writing to:
+//!
+//!     Free Software Foundation, Inc. 
+//!     59 Temple Place - Suite 330
+//!     Boston, MA 02111-1307
+//!     USA
+//!
+//! ---------------------------------------------------------------------------
+//!
+//! Permission is granted to copy, distribute and/or modify this documentation
+//! under the terms of the GNU Free Documentation License, Version 1.1 or any
+//! later version published by the Free Software Foundation with no Invariant
+//! Sections, no Front-Cover Texts, and no Back-Cover Texts. You may obtain a
+//! copy of the GNU Free Documentation License from the Free Software
+//! Foundation by visiting their Web site or by writing to the address above.
+
 extern crate cairo;
 extern crate libc;
 
@@ -13,18 +45,27 @@ pub mod scaling;
 pub mod stage;
 pub mod text;
 
+/// Initialises everything needed to operate with Clutter.
+///
+/// It is safe to call this function multiple times.
+///
+/// This function will not abort in case of errors during initialization;
+/// `init()` will print out the error message on stderr, and will return an
+/// error code. It is up to the application code to handle this case.
 pub fn init() {
   unsafe {
     clutter_init(std::ptr::null_mut(), std::ptr::null_mut());
   }
 }
 
+/// Starts the Clutter mainloop.
 pub fn main() {
   unsafe {
     clutter_main();
   }
 }
 
+/// Terminates the Clutter mainloop.
 pub fn main_quit() {
   unsafe {
     clutter_main_quit();
