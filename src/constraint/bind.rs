bd54bf9949ebaf6e923273e7630802e2321d1b81
Félix Saparelli <felix@passcod.name>
2014-10-27 21:33:07 +1300

    Document constraint mod

--- src/constraint/bind.rs
+++ src/constraint/bind.rs
@@ -1,10 +1,34 @@
+#![stable]
+
+/// Specifies which property should be used in a binding.
+///
+/// _Since 1.4_
 #[repr(i32)]
 pub enum Coordinate {
+  /// Bind the X coordinate
   X = 0,
+
+  /// Bind the Y coordinate
   Y = 1,
+
+  /// Bind the width
   Width = 2,
+
+  /// Bind the height
   Height = 3,
+
+  /// Equivalent to X and Y
+  ///
+  /// _Since 1.6_
   Position = 4,
+
+  /// Equivalent to Width and Height
+  ///
+  /// _Since 1.6_
   Size = 5,
+
+  /// Equivalent to Position and Size
+  ///
+  /// _Since 1.10_
   All = 6
 }
--- src/constraint/mod.rs
+++ src/constraint/mod.rs
@@ -1,17 +1,25 @@
+#![stable]
+
 use libc;
 use super::actor::Actor;
 use super::actor::ActorMeta;
 
 pub mod bind;
 
+/// Opaque struct which holds a reference to the underlying Clutter object.
 #[repr(C)]
 pub struct ConstraintRef {
   opaque: *mut libc::c_void
 }
 
+/// Trait for constraints on position or size.
+///
+/// _Since 1.4_
 pub trait Constraint {
+  /// Returns a pointer to the the underlying C object.
+  ///
+  /// Generally only used internally.
   fn as_constraint(&self) -> *mut libc::c_void;
-
 }
 
 impl Constraint for ConstraintRef {
@@ -26,12 +34,17 @@ impl ActorMeta for ConstraintRef {
   }
 }
 
+/// Opaque struct which holds a reference to the underlying Clutter object.
 #[repr(C)]
 pub struct BindConstraintRef {
   opaque: *mut libc::c_void
 }
 
 impl BindConstraintRef {
+  /// Creates a new constraint, binding an Actor's position to the given
+  /// coordinate of the position of source.
+  ///
+  /// _Since 1.4_
   pub fn new<T: Actor>(source: &mut T, coordinate: bind::Coordinate, offset: f32) -> BindConstraintRef {
     unsafe {
       let foreign_result = clutter_bind_constraint_new(source.as_actor(), coordinate, offset);
@@ -40,9 +53,21 @@ impl BindConstraintRef {
   }
 }
 
+/// A constraint binding the position or size of an actor.
+///
+/// BindConstraint is a Constraint that binds the position or the size of the
+/// Actor to which it is applied to the position or the size of another Actor,
+/// or "source".
+///
+/// An offset can be applied to the constraint, to avoid overlapping.
+/// The offset can also be animated.
+///
+/// _Since 1.4_
 pub trait BindConstraint {
+  /// Returns a pointer to the the underlying C object.
+  ///
+  /// Generally only used internally.
   fn as_bind_constraint(&self) -> *mut libc::c_void;
-
 }
 
 impl BindConstraint for BindConstraintRef {
